17934338da21509e99b4cfdf1d37edb40644ff07
pallavika11 <gampalapallavika@gmail.com>
2024-02-28 19:03:27 +0530

    first commit

---
+++ src/main/java/com/cisco/crud/dao/CustomerDAO.java
@@ -0,0 +1,15 @@
+package com.cisco.crud.dao;
+
+import java.util.List;
+
+import com.cisco.crud.model.Customer;
+
+public interface CustomerDAO {
+	
+public Customer createCustomer(Customer cust);
+public List<Customer> getAllCustomers();
+public void removeCustomer(int id);
+
+
+	
+}
